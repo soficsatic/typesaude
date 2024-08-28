fc6fdaecabffb93ce46ef8ef0cc3807a3d69aeca
Julio <jcvieiradev@gmail.com>
2024-08-28 20:02:42 -0300

    Criada a tela de login

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,7 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="h-screen bg-[url('/images/authbg.webp')] bg-cover bg-center">
+      {children}
+    </main>
+  );
+}
